dd5b7c33e943a07bf74d3db56b31e618c5a26737
Rohith <rohithsuresh207@gmail.com>
2024-10-31 12:05:50 +0530

    word parsing working, fixes to some base parsers

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,14 +1,54 @@
 type Parser[T] = (String) => ParserResult[T]
 type ParserResult[T] = List[(T, String)]
 
-def result[T](in: T): String => ParserResult[T] = (inStr: String) =>
-  List((in, inStr))
-def zero[T](in: T): String => ParserResult[T] = (inStr: String) => List()
-def item[char](in: char): String => ParserResult[char] = (inStr: String) =>
+def result[T](in: T): Parser[T] = (inStr: String) => List((in, inStr))
+def zero[T](): Parser[T] = (inStr: String) => List()
+val item: Parser[Char] = (inStr: String) =>
   inStr match {
-    case u if inStr.startsWith(in.toString()) =>
-      List((in, inStr.substring(1, inStr.length())))
-    case _ => List[(char, String)]()
+    case u if u.length == 0 => List()
+    case u                  => List((u(0), u.substring(1, u.length())))
   }
 
-  def bind[T, W](p: Parser[T], f: (T => Parser[W])): Parser[W] =
+def bind[T, W](p: Parser[T], f: T => Parser[W]): Parser[W] = { input =>
+  val results = p(input)
+  results
+    .flatMap { case (value, remaining) =>
+      f(value)(remaining)
+    }
+}
+
+val sat = (p: Char => Boolean) =>
+  (inStr: String) =>
+    bind(
+      item,
+      (x: Char) =>
+        if p(x) then result(x)
+        else zero()
+    )(inStr)
+
+val parseDigit: Parser[Char] = sat(((x: Char) => x.isDigit))
+val parseLower: Parser[Char] = sat(((x: Char) => x.isLower))
+val parseUpper: Parser[Char] = sat(((x: Char) => x.isUpper))
+
+def plus[T](p: Parser[T], q: Parser[T]): Parser[T] = (inStr: String) =>
+  p(inStr) ++ q(inStr)
+
+def or[T](p: Parser[T], q: Parser[T]): Parser[T] = { inStr =>
+  plus(p, q)(inStr) match {
+    case Nil     => List()
+    case x :: xs => List(x)
+  }
+}
+
+val char = or(parseLower, parseUpper)
+
+val word: Parser[String] = (inStr: String) =>
+  val initChar = char(inStr)
+  if initChar.length == 0 then List(("", inStr))
+  else {
+    val rest = word(initChar(0)(1))
+    if rest.length == 0 then List((initChar(0)(0).toString(), initChar(0)(1)))
+    else List((initChar(0)(0).toString().concat(rest(0)(0)), rest(0)(1)))
+  }
+
+//def sat(condition: char => bool) = (inStr: String)=>
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -2,6 +2,19 @@
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
   test("parse start array token") {
-    assert(item('d')("done") == List(('d', "one")))
+    assert(item("done") == List(('d', "one")))
+    assert(parseDigit("3c") == List(('3', "c")))
+    assert(parseDigit("c3") == List())
+    assert(parseLower("c3") == List(('c', "3")))
+    assert(parseUpper("C3") == List(('C', "3")))
+    var parseTwoLower = bind(
+      parseLower,
+      (x: Char) => bind(parseLower, (y: Char) => result(x, y))
+    )
+    assert(parseTwoLower("cdE") == List((('c', 'd'), "E")))
+    assert(or(parseLower, parseUpper)("Cd") == List(('C', "d")))
+    assert(or(parseLower, parseUpper)("dC") == List(('d', "C")))
+    assert(char("abcd") == List(('a', "bcd")))
+    assert(word("abcd") == List(("abcd", "")))
   }
 }
